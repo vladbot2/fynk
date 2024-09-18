8a438dfa7feb6e909c504ec00e516abd04b34ca3
dev <dev@A21PC12>
2024-09-18 17:23:07 +0300

    Add project files.

---
+++ fynk.cpp
@@ -0,0 +1,175 @@
+﻿using namespace std;
+#include <iostream>
+
+int main() {
+	//1
+	//int N, K;
+	//cout << " N: ";
+	//cin >> N;
+	//cout << " K: ";
+	//cin >> K;
+
+	//for (int i = 0; i < N; ++i) {
+	//	for (int j = 0; j < K; ++j) {
+	//		cout << '*'; 
+	//	}
+	//	cout << '\n'; 
+	//}
+	
+
+	//2
+	//int factorial(int n); {
+	//	int result = 1;
+	//	for (int i = 1; i <= n; ++i) {
+	//		result *= i;
+	//	}
+	//	return result;
+	//}
+	//	int n;
+	//	cout << "Enter a number: ";
+	//	cin >> n;
+	//	cout << "Factorial of " << n << " is " << factorial(n) << '\n';
+	
+	//3
+	//int factorial(int n); {
+	//	int result = 1;
+	//	for (int i = 1; i <= n; ++i) {
+	//		result *= i;
+	//	}
+	//	return result;
+	//}
+
+	//	int n;
+	//	cout << "Enter a number: ";
+	//	cin >> n;
+
+	//	cout << "Factorial of " << n << " is " << factorial(n) << '\n';
+
+	//5
+	//int max(int min, int max);
+	//	int num1, num2;
+	//	cout << "Enter two numbers: ";
+	//	cin >> num1 >> num2;
+	//	cout << "The largest number is: " << max(num1, num2) << endl;
+	//}
+	//int max(int min, int max)
+	//{
+	//	if (min > max)
+	//		return min;
+	//	else
+	//		return max;
+	//
+	
+	//4
+	//int cube(int number); {
+	//	return number * number * number;
+	//}
+	//	int num;
+
+	//	cout << "Enter a number: ";
+	//	cin >> num;
+
+	//	cout << "The cube of a number " << num << " = " << cube(num) << endl;
+
+
+	//6
+	//bool isPositive(int number); {
+	//	return number > 0; 
+	//}
+
+	//	int num;
+
+	//	cout << "Enter a number: ";
+	//	cin >> num;
+
+	//	if (isPositive(num)) {
+	//		cout << "The number " << num << " is positive." << endl;
+	//	}
+	//	else {
+	//		cout << "The number " << num << " is negative or zero." << endl;
+	//	}
+
+
+
+
+
+
+
+
+//void printMEssage()
+//{
+//	cout << "Hello, World" << endl;
+//}
+//void printMessageFromUser(int age) 
+//
+//{
+//	age++;
+//	cout << "Hello, yuo are " << age << "years old" << endl;
+//	age++;
+//}
+//
+//void paintStar()
+//{
+//	cout << " *" << "";
+//}
+//void countStars(int count)
+//{
+//	for (int i = 0; i < count; i++)
+//	{
+//		paintStar();
+//	}
+//}
+//void showSymbols(int count = 5, char sym = '#')
+//{
+//	for (int i = 0; i < count; i++)
+//	{
+//		cout << sym;
+//	}
+//}
+//
+//int printUsersAge(int age)
+//{
+//	age += 10;
+//	if (age > 100) {
+//	return age;
+//}
+//	else {
+//		return age;
+//	}
+//}
+//void showSumOfArr(int arr[])
+//{int sum = 0
+//	for (int i = 0; i < size; i++)
+//	{
+//		sum += arr[i]
+//	}
+//	cout << "Sum of array is:" << sum << endl;
+//}
+//
+//
+
+//{
+//
+//	int arr2[]{ 1,2,3,4,5,6,7,8,9,10,67,76,67,676,76,76, };
+//	int size = sizeof(arr2) / sizeof(arr[0]);
+//	showSumOfArr(arr2, size);
+	//int num;
+	//char symbol;
+	//cout << "Enter number of symbols :";
+	//cin >> num;
+	////cout << "Enter symbol :";
+	////cin >> symbol;
+	//showSymbols(num, symbol);
+	//countStars(5);
+
+
+
+	//int b = 33;
+	//printMEssage();
+	//printMessageFromUser(b);
+	//cout << "b is " << b << endl;
+	//cout << "age of yser :" << printUsersAge(b) << endl;
+	//int age = printUsersAge(66);
+	//cout << age << endl;
+}
+
